20b5d6ad9229b3fbe283b33c5f4d84b7bc7b805f
kay.grebenstein <kay.grebenstein@NB388.saxsys.de>
2016-04-08 09:16:19 +0200

    tests sollen laufen

---
+++ AddressApp/src/test/java/de/saxsys/training/javafx/adress/test/BLA_ST.java
@@ -0,0 +1,13 @@
+package de.saxsys.training.javafx.adress.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BLA_ST {
+
+    @Test
+    public void test() {
+    }
+
+}
