50c3fbc3800ecaa91003b76db78aea81fea2a82f
kay.grebenstein <kay.grebenstein@NB388.saxsys.de>
2016-05-09 11:17:03 +0200

    screenshots

--- AddressApp/src/test/java/de/saxsys/training/javafx/adress/test/Test_Beispiel_IT.java
+++ AddressApp/src/test/java/de/saxsys/training/javafx/adress/test/Test_Beispiel_IT.java
@@ -1,13 +1,52 @@
 package de.saxsys.training.javafx.adress.test;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
+
 import org.testfx.api.FxAssert;
 import org.testfx.api.FxRobot;
+import org.testfx.api.FxService;
 import org.testfx.api.FxToolkit;
 import org.testfx.matcher.base.NodeMatchers;
 import org.testfx.matcher.control.LabeledMatchers;
+
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import com.google.common.base.Predicate;
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+import org.testfx.service.finder.NodeFinder;
+import org.testfx.service.finder.NodeFinderException;
+import org.testfx.service.finder.WindowFinder;
+import org.testfx.service.support.CaptureSupport;
+import org.testfx.service.support.WaitUntilSupport;
+import org.testfx.util.WaitForAsyncUtils;
+
+import static org.junit.Assume.assumeFalse;
+import static org.testfx.api.FxService.serviceContext;
+
 
 import de.saxsys.training.javafx.address.MainApp;
 import javafx.scene.control.Button;
@@ -18,11 +57,20 @@ import org.testfx.util.WaitForAsyncUtils;
 
 public class Test_Beispiel_IT extends FxRobot  {
 		
+    private static final CaptureSupport captureSupport = serviceContext().getCaptureSupport();
+    
 	@Before
     public void setup() throws Exception {
 		FxToolkit.registerPrimaryStage();
 		FxToolkit.setupApplication(MainApp.class);
 		FxToolkit.showStage();				
+    } 
+	
+	public static File captureScreenshot(String text) {
+        Path captureFile = Paths.get("screenshot_" + text + "_" + new Date().getTime() + ".png");
+        Image captureImage = captureSupport.captureRegion(Screen.getPrimary().getBounds());
+        captureSupport.saveImage(captureImage, captureFile);
+        return captureFile.toFile();
     }
 
 	@Test
@@ -36,8 +84,12 @@ public class Test_Beispiel_IT extends FxRobot  {
         doubleClickOn("#streetField").write("Fritz-Foerster-Platz 2");    
         clickOn("#Ok");
         
+        Test_Beispiel_IT.captureScreenshot("new_Entry");
+        
         WaitForAsyncUtils.waitForFxEvents();
         
+        Test_Beispiel_IT.captureScreenshot("new_Entry");
+                
         // then:
         FxAssert.verifyThat("Kay", LabeledMatchers.hasText("Kay"));
         FxAssert.verifyThat("Grebenstein", LabeledMatchers.hasText("Grebenstein"));
@@ -63,7 +115,11 @@ public class Test_Beispiel_IT extends FxRobot  {
     	final Button button_Ok = lookup("#Ok").query();
     	clickOn(button_Ok);
     	
+        Test_Beispiel_IT.captureScreenshot("click_Edit");
+
     	WaitForAsyncUtils.waitForFxEvents();
+    	
+    	Test_Beispiel_IT.captureScreenshot("click_Edit");
     	               
         // then:
         FxAssert.verifyThat("Müller", LabeledMatchers.hasText("Müller"));
@@ -74,6 +130,8 @@ public class Test_Beispiel_IT extends FxRobot  {
         FxAssert.verifyThat("Kunz", NodeMatchers.isNotNull());
         final TableCell test_cell_Kunz = lookup("Kunz").query();
         clickOn(test_cell_Kunz);
+        
+        Test_Beispiel_IT.captureScreenshot("loesche_Eintrag");
         
         FxAssert.verifyThat("#Delete", NodeMatchers.isNotNull());
         final Button button_delete = lookup("#Delete").query();
@@ -88,3 +146,4 @@ public class Test_Beispiel_IT extends FxRobot  {
         
     }*/
 }
+
